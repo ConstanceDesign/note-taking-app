cc645e2e133ee049ba7a198eb4d637d1b336e622
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-02-06 20:33:25 -0500

    heroku

--- server.js
+++ server.js
@@ -1,21 +1,70 @@
+// route that the front-end can request data from
+const { notes } = require("./db/db.json");
+
+// require express
 const express = require("express");
+
+// const fs = require("fs");
+// const path = require("path");
+// const notes = require("./db/db.json");
+// const uuid = require("uuid");
+
+// instantiate the server
+const app = express();
+
+// routes
 const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
 
-const app = express();
-const PORT = 3000;
+// add the port
+const PORT = 3001;
 
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/", htmlRoutes);
+app.use("/api", apiRoutes);
+
+// saves notes into db.json
 app.get("/api/notes", (req, res) => {
   const data = JSON.parse(fs.readFileSync("./db/db.json"));
   console.log(data);
   res.json(data);
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use("/", htmlRoutes);
-app.use("/api", apiRoutes);
+// adds new notes
+app.post("/api/notes", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+  const newNote = req.body;
+  newNote.id = uuid.v4();
+  notes.push(newNote);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+  res.json(notes);
+});
+
+// deletes notes
+app.delete("/api/notes/:id", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+  const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+  fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
+  res.json(delNote);
+});
+
+// html home request
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+//html notes request
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// listen for requests
+// app.listen(3001, () => {
+//   console.log(`API server now on port 3001!`);
+// });
 
 app
   .listen(PORT, () => console.log(`Listening on PORT: ${PORT}`))
